d5515b5a3fe4c6d7861833c557f5d7b54c9448f5
Marton Borlay <borlaym@gmail.com>
2018-08-19 16:37:04 +0200

    css monster health

--- src/components/ColorCircle.tsx
+++ src/components/ColorCircle.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import './ColorCircle.css';
 import Color from '../models/Color';
 
-export default function ColorCircle(props: { color: Color }) {
-	return <div className="circle" style={{ backgroundColor: props.color }} />
+export default function ColorCircle(props: { color: Color, large?: boolean }) {
+	return <div className={`circle ${props.color} ${props.large && 'large'}`} />
 }
--- src/components/MonsterHealth.tsx
+++ src/components/MonsterHealth.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import ColorCircle from './ColorCircle';
 import Color from '../models/Color';
+import './MonsterHealth.css';
 
 export default function MonsterHealth(props: { colors: Color[] }) {
 	return (
 		<div className="monster-health">
-			{props.colors.map((c, i) => <ColorCircle key={i} color={c} />)}
+			{props.colors.map((c, i) => <ColorCircle key={i} color={c} large={i === 0} />)}
 		</div>
 	)
 }
