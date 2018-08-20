768cdffcb1392d2907fe10697e3b95db32fb116b
Marton Borlay <borlaym@gmail.com>
2018-08-20 09:02:31 +0200

    Taunt

--- src/reducers/monsterIntent.ts
+++ src/reducers/monsterIntent.ts
@@ -4,8 +4,18 @@ import MonsterIntent from '../models/MonsterIntent'
 
 export default function MonsterIntentReducer(state: MonsterIntent, action: ReduxAction<any>, monster: Monster) {
 	switch (action.type) {
-		case ActionType.ATTACK:
+		case ActionType.ATTACK: {
+			if (action.payload.action.effect) {
+				switch (action.payload.action.effect) {
+					case 'Taunt': {
+						return new MonsterIntent(state.attack, state.turnsLeft - 1, action.payload.character)
+					}
+					default:
+						return new MonsterIntent(state.attack, state.turnsLeft - 1, state.target)
+				}
+			}
 			return new MonsterIntent(state.attack, state.turnsLeft - 1, state.target)
+		}
 		default:
 			return state
 	}
